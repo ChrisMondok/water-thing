605a911563abac272761943f089c698509cdf4a3
Chris Mondok <chris.mondok@gmail.com>
2015-10-11 09:19:46 -0400

    Clean up extensions, fix indenting

--- extensions.js
+++ extensions.js
@@ -15,16 +15,17 @@ Vector.prototype.normalize = function() {
 
 Array.prototype.flatten = function() {
 	//yeah, this is ugly, but it's much faster than array.reduce(concat)
-	var count = this.reduce(function(count, a) {
-		return count + a.length;
-	}, 0);
+	var i = 0, count = 0;
+	for(i = 0; i < this.length; i++)
+		count += this[i].length;
 
 	var output = new Array(count);
 
 	var o = 0;
-	for(var iA = 0; iA < this.length; iA++)
-		for(var i = 0; i < this[iA].length; i++)
+	for(var iA = 0; iA < this.length; iA++) {
+		for(i = 0; i < this[iA].length; i++)
 			output[o++] = this[iA][i];
+	}
 
 	return output;
 };
