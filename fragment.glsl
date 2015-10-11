52ef7a3ea4b0d5b05d715bb537205d5e70a4b283
Chris Mondok <chris.mondok@gmail.com>
2015-10-11 17:12:28 -0400

    Functions should be verbs

--- fragment.glsl
+++ fragment.glsl
@@ -14,19 +14,23 @@ varying vec3 v_vertex_pos;
 
 const float screen_gamma = 2.2;
 
-mediump vec3 diffuse() {
-	return u_diffuse * max(dot(normalize(v_vertex_normal), u_sun), 0.0);
+mediump float compute_lambertian() {
+	return max(dot(normalize(v_vertex_normal), u_sun), 0.0);
 }
 
-mediump vec3 ambient() {
+mediump vec3 compute_diffuse() {
+	return u_diffuse * compute_lambertian();
+}
+
+mediump vec3 compute_ambient() {
 	return u_diffuse * u_ambient_light;
 }
 
-mediump vec3 specular() {
+mediump vec3 compute_specular() {
 	vec3 normal = normalize(v_vertex_normal);
 	vec3 light_direction = normalize(u_sun);
 
-	float lambertian = max(dot(light_direction, normal), 0.0);
+	float lambertian = compute_lambertian();
 	float power = 0.0;
 
 	if(lambertian > 0.0 || lambertian <= 0.0) {
@@ -42,7 +46,7 @@ mediump vec3 specular() {
 }
 
 void main() {
-	vec3 color_linear = ambient() + diffuse() + specular();
+	vec3 color_linear = compute_ambient() + compute_diffuse() + compute_specular();
 	vec3 color_gamma_corrected = pow(color_linear, vec3(1.0/screen_gamma));
 	gl_FragColor = vec4(color_gamma_corrected, 1);
 }
