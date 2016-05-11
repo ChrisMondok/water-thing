ee0b0ad53bd383fbf50888bdbfaf1743fd018d3d
Chris Mondok <chris.mondok@gmail.com>
2016-05-10 22:29:06 -0400

    Brighten it up

--- shaders/radarFragment.glsl
+++ shaders/radarFragment.glsl
@@ -9,12 +9,12 @@ varying float v_distance;
 void main() {
 	float pulseWidth = 5.0;
 
-	float pulseIntensity = 1.0 - min(1.0, max(0.0, abs(v_distance - u_pulse_radius)/pulseWidth));
+	float pulseIntensity = 1.1 * (1.0 - min(1.0, max(0.0, abs(v_distance - u_pulse_radius)/pulseWidth)));
 
 	float lingerIntensity = 0.0;
 	if(v_distance < u_pulse_radius)
-		lingerIntensity = 0.6 * (v_distance + 100.0 - u_pulse_radius) / 100.0;
+		lingerIntensity = max(0.0, 0.6 * (v_distance + 200.0 - u_pulse_radius) / 200.0);
 
 	float i = pulseIntensity + lingerIntensity;
-	gl_FragColor = vec4(0, i, 0, i);
+	gl_FragColor = vec4(i-1.0, i, i-1.0, i);
 }
