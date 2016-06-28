8f8d324952e7fd1395a0d512a9a74544a4457c92
Chris Mondok <chris.mondok@gmail.com>
2016-06-28 19:05:53 -0400

    Stick a motor on the boat, and a prop on that!

--- Boat.js
+++ Boat.js
@@ -1,39 +1,67 @@
 function Boat(gl, water) {
-	Actor.apply(this);
+	Actor.apply(this)
 
-	this.water = water;
+	this.water = water
 
-	vec3.set(this.scale, 15, 15, 15);
+	vec3.set(this.scale, 15, 15, 15)
 
-	this.createComponents();
+	this.createComponents()
 }
-Boat.meshes = null;
 
-Boat.prototype = Object.create(Actor.prototype);
-Boat.prototype.constructor = Boat;
+Boat.prototype = Object.create(Actor.prototype)
+Boat.prototype.constructor = Boat
+
+Boat.prototype.steeringAngle = 0
 
 Boat.prototype.createComponents = function() {
-	if(!Boat.meshes)
-		throw new Error("Boat instantiated before mesh was set.");
+	loadMesh("models", "dinghy.obj").then(function(meshes) {
+		meshes.forEach(function(m) {
+			var mesh = new StaticMeshComponent(m)
+			vec3.set(mesh.position, 0, 0, 0.1)
+			this.addComponent(mesh)
+		}, this)
+	}.bind(this))
 
-	Boat.meshes.forEach(function(m) {
-		var mesh = new StaticMeshComponent(m);
-		vec3.set(mesh.position, 0, 0, 0.1);
-		this.addComponent(mesh);
-	}, this);
+	var motor = this.motor = new SceneGraphNode()
 
-};
+	vec3.set(motor.position, 0, -2.1, 0)
 
-(function() {
+	this.addComponent(motor)
+
+	loadMesh("models", "outboard-motor.obj").then(function(meshes) {
+		meshes.forEach(function(mesh) {
+			motor.addComponent(new StaticMeshComponent(mesh))
+		})
+	})
+
+
+	var prop = this.prop = new SceneGraphNode()
+	vec3.set(prop.position, 0, -0.1, -0.8)
+	motor.addComponent(prop)
+
+	loadMesh("models", "prop.obj").then(function(meshes) {
+		meshes.forEach(function(mesh) {
+			prop.addComponent(new StaticMeshComponent(mesh))
+		}, this)
+	}.bind(this))
+}
+
+;(function() {
 	var up = vec3.fromValues(0, 0, 1)
+
+	var noRotation = quat.create()
 
 	var normal = vec3.create()
 	Boat.prototype.tick = function(timestamp) {
-		var xy = [this.x, this.y];
-		this.z = this.water.getZ(timestamp, xy);
+		var xy = [this.x, this.y]
+		this.z = this.water.getZ(timestamp, xy)
 
-		this.water.getNormal(normal, timestamp, xy);
+		this.water.getNormal(normal, timestamp, xy)
 
 		quat.rotationTo(this.rotation, up, normal)
-	};
+
+		quat.rotateZ(this.motor.rotation, noRotation, this.steeringAngle)
+
+		quat.rotateY(this.prop.rotation, noRotation, (timestamp / 500) % 2 * Math.PI)
+	}
 })()
