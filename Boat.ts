d6141c910636f14b9ba536dcc93e767925a30989
Chris Mondok <chris.mondok@gmail.com>
2017-03-15 20:42:09 -0400

    Fix call to Float32Array.set

--- Boat.ts
+++ Boat.ts
@@ -54,7 +54,8 @@ class Boat extends Actor {
   }
 
   tick() {
-    Boat.scratch.xy.set(this.x, this.y)
+    Boat.scratch.xy[0] = this.x;
+    Boat.scratch.xy[1] = this.y;
     this.z = this.water.getZ(this.game.now, Boat.scratch.xy)
 
     this.water.getNormal(Boat.scratch.normal, this.game.now, Boat.scratch.xy)
